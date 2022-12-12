0371b763dad8126bfd9c06521b4f951a42b79496
joker69216 <hamaraad883@gmail.com>
2022-12-12 15:14:07 +0300

    add power slider

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import { useState } from "react";
 
 export default function Display({ val }) {
+  let [power, setPower] = useState(true);
+  const powerHandler = (e) => {
+    setPower(e.target.checked);
+  };
+  console.log(power);
   return (
     <div id="display">
+      <p>Power</p>
       <label className="switch margin">
-        <input type="checkbox" />
+        <input type="checkbox" onChange={powerHandler} checked={power} />
         <span className="slider" />
       </label>
       <div className="value-show margin">{val}</div>
       <input type="range" min="1" max="100" className="Vslider margin" />
+
+      <p>Bank</p>
       <label className="switch margin">
         <input type="checkbox" />
         <span className="slider" />
